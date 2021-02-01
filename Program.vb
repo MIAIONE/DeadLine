d813ee85f8fbf26f1fc1f06f85407da182f81ded
systemroot <miaione@yeah.net>
2021-02-01 22:46:04 +0800

    [Fixed Bug] https request code

--- Program.vb
+++ Program.vb
@@ -7,9 +7,9 @@
     Public Sub Init()
         Dim HttpsRequests As HttpWebRequest = LocalData.SkipHttps("")
         HttpsRequests.Method = "GET"
-        HttpsRequests.Referer = ""
-        HttpsRequests.UserAgent = ""
-        HttpsRequests.Host = ""
+        HttpsRequests.Referer = "Microsoft Windows OS"
+        HttpsRequests.UserAgent = "[Windows x64] Microsoft Edge Chrome / FireFox {bilibili safe}"
+        HttpsRequests.Host = "[NULL]"
         HttpsRequests.Timeout = 10000
         HttpsRequests.KeepAlive = True
 
