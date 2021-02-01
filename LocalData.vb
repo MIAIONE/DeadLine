cd9ffe2ad6dc857c80bdb2385e7b8a350f68d4a4
systemroot <miaione@yeah.net>
2021-02-01 22:41:16 +0800

    添加项目文件。

---
+++ LocalData.vb
@@ -0,0 +1,20 @@
+﻿
+
+Public Class LocalData
+
+    Public Shared Function SkipHttps(url As String) As HttpWebRequest
+        Dim request As HttpWebRequest
+        If url.StartsWith("https://", StringComparison.CurrentCultureIgnoreCase) Then
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13 Or SecurityProtocolType.Tls12 Or SecurityProtocolType.Tls11 Or SecurityProtocolType.Tls Or SecurityProtocolType.Ssl3
+            ServicePointManager.ServerCertificateValidationCallback = New RemoteCertificateValidationCallback(AddressOf CheckValidationResult)
+            request = WebRequest.Create(url)
+            request.ProtocolVersion = HttpVersion.Version11
+        Else
+            request = WebRequest.Create(url)
+        End If
+        Return request
+    End Function
+    Private Shared Function CheckValidationResult(sender As Object, certificate As X509Certificate, chain As X509Chain, errors As SslPolicyErrors) As Boolean
+        Return True
+    End Function
+End Class
---
+++ Program.vb
@@ -0,0 +1,18 @@
+﻿Public Class Program
+
+    Public Sub Main()
+        Init()
+    End Sub
+
+    Public Sub Init()
+        Dim HttpsRequests As HttpWebRequest = LocalData.SkipHttps("")
+        HttpsRequests.Method = "GET"
+        HttpsRequests.Referer = ""
+        HttpsRequests.UserAgent = ""
+        HttpsRequests.Host = ""
+        HttpsRequests.Timeout = 10000
+        HttpsRequests.KeepAlive = True
+
+    End Sub
+
+End Class
